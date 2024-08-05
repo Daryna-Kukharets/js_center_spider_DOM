a6f0824becedf67c4950906707fd4ac5a488bdff
Daryna Kukharets <darinakuharec61@gmail.com>
2024-08-06 00:44:07 +0300

    make build

---
+++ index.aa451d22.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2,l=e.clientHeight/2,o=t.offsetWidth/2;t.style.left="".concat(c-o,"px"),t.style.top="".concat(l-o,"px");
+//# sourceMappingURL=index.aa451d22.js.map
